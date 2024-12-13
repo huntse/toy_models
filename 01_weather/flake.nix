b517064900c460985c8cf1dc6426617072d96e2c
huntse <sean@uncarved.com>
2024-12-13 19:07:53 +0000

    Move to nix stable so we pin to a specific version

--- 01_weather/flake.nix
+++ 01_weather/flake.nix
@@ -1,32 +1,37 @@
 {
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "github:nixos/nixpkgs/nixos-24.11";
     };
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
   };
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          wxmaxima
-          gnuplot
-          (python3.withPackages(ps: with ps; [
-            ipython
-            jupyter
-            numpy
-            matplotlib
-          ]))
-        ];
-#        shellHook = "jupyter notebook";
-      };
-    }
-  );
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            wxmaxima
+            gnuplot
+            (python3.withPackages (ps:
+              with ps; [
+                ipython
+                jupyter
+                numpy
+                matplotlib
+              ]))
+          ];
+          #        shellHook = "jupyter notebook";
+        };
+      }
+    );
 }
-
